993d05780837cd9e9089e75b1e3a1fd7f0de0e5b
Lees <leesasp@gmail.com>
2013-11-24 23:33:43 +0400

    issue4
    * moved from int to long (able to get to 10^6 primes)
    * added primes module to get ith prime

---
+++ Java/Primes.java
@@ -0,0 +1,41 @@
+
+public class Primes
+{
+	
+	//private SundaramExclude ex_generator;
+
+	// Primes()
+	// {
+	// 	ex_generator = new SundaramExclude();
+	// }
+
+	public static long ithprime(long n)
+	{
+		if (n<3) return n;
+
+		SundaramExclude ex_generator = new SundaramExclude();
+		long current = 1;
+		long to_exclude = ex_generator.next();
+
+		while (n>2)
+		{
+			current++;
+			if (current<to_exclude)
+			{
+				n--;
+			}
+			else
+			{
+				to_exclude = ex_generator.next();
+			}
+		}
+		return 2*current+1;
+	}
+
+
+	public static void main(String[] args) 
+	{
+		System.out.println(ithprime(Integer.parseInt(args[0])));
+	}
+
+}
